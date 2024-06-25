33e204c5d7aa41b244f87cee097244fc35b1acd0
sasi <sasikumar.s@cogniverselabs.com>
2024-06-25 11:05:51 +0530

    org details fix

--- v2_app.py
+++ v2_app.py
@@ -73,7 +73,7 @@ def get_issues_by_owner_id_v2(owner, issue):
             "mentor_id": dmp_issue_id['mentor_username'] ,
             "contributor":define_link_data(cont_details),
             # "contributor_id": cont_details[0]['contributor_id'],
-            "org": define_link_data(dmp_issue_id['mentor_username'])[0] if dmp_issue_id['mentor_username'] else [],
+            "org": define_link_data(repo_owner),
             "weekly_goals_html": w_goal_url,
             "weekly_learnings_html": w_learn_url,
             "overall_progress":calculate_overall_progress(week_data,12),
