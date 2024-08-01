4a28e189b4b4be7f1ef8bb60d148e83e972824a2
sasi <sasikumar.s@cogniverselabs.com>
2024-08-01 12:46:00 +0530

    query convertion into sql

--- db.py
+++ db.py
@@ -3,6 +3,9 @@ from typing import Any
 from supabase import create_client, Client
 from supabase.lib.client_options import ClientOptions
 from abc import ABC, abstractmethod
+import psycopg2,json
+from psycopg2.extras import RealDictCursor
+
 
 client_options = ClientOptions(postgrest_client_timeout=None)
 
@@ -36,6 +39,39 @@ class SupabaseInterface():
             SupabaseInterface._instance = SupabaseInterface()
         return SupabaseInterface._instance
     
+    
+    def get_postgres_connection():
+        
+        # Database configuration
+        DB_HOST =os.getenv('POSTGRES_DB_HOST')
+        DB_NAME =os.getenv('POSTGRES_DB_NAME')
+        DB_USER =os.getenv('POSTGRES_DB_USER')
+        DB_PASS =os.getenv('POSTGRES_DB_PASS')
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS
+        )
+        return conn
+    
+    def postgres_query(query,params=None):        
+        conn = SupabaseInterface.get_postgres_connection()
+
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+        # cursor = conn.cursor()
+        if not params:
+            cursor.execute(query)
+        else:
+            cursor.execute(query,params)
+            
+        rows = cursor.fetchall()
+        results_as_dicts = [dict(row) for row in rows]
+
+        cursor.close()
+        conn.close()
+        return results_as_dicts
        
     def readAll(self, table):
         data = self.client.table(f"{table}").select("*").execute()
